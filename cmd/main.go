58a3fc997906965b5f291f57db0846df0591f925
kitsdk@163.com <kitsdk@163.com>
2021-02-05 16:23:23 +0800

    di

--- cmd/main.go
+++ cmd/main.go
@@ -37,10 +37,26 @@ func bootInvoke(ctx context.Context, client *qmgo.QmgoClient, conf *model.Config
 
 }
 func main() {
+
 	fx.New(
 		fx.Provide(model.ProvideConfig),
 		fx.Provide(model.ProvideLog),
 		fx.Provide(model.ProvideDbClient),
 		fx.Invoke(bootInvoke),
 	)
+	/*
+		scm := di.ServiceConstructorMap{
+			"config": func(get di.Get) interface{} {
+				return model.ProvideConfig()
+			},
+			"log": func(get di.Get) interface{} {
+				return model.ProvideLog(get("config").(*model.Config))
+			},
+		}
+
+		container := di.NewContainer(scm)
+		config := container.Get("config").(*model.Config)
+		log := container.Get("log").(*log.Logger)
+		log.Println(config.Area)
+	*/
 }
